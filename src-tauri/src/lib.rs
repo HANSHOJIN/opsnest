e012fa337d3f1ebbe9ef5a32c923daeea55474be
OpsNest Contributors <opsnest@users.noreply.github.com>
2026-07-23 11:08:45 +0800

    chore: set initial alpha version

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[tauri::command]
-fn app_version() -> &'static str { "0.1.0" }
+fn app_version() -> &'static str { "0.1.0-alpha.1" }
 
 pub fn run() {
     tauri::Builder::default()
